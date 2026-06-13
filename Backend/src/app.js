e493a1a6e54bb43539834c25c48cbe8e2c896257
Harshu Chaturvedi <hchaube7@gmail.com>
2026-06-13 21:07:28 +0530

    Fix CORS origin exact domain name

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cookieParser())
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://atsfreak.vercel.app" 
+    "https://ats-freak.vercel.app" 
 ];
 
 app.use(cors({
