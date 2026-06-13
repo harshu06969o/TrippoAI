d091b9dd9ce95c196973897813ded6dc1d82100c
Harshu Chaturvedi <hchaube7@gmail.com>
2026-06-13 20:50:19 +0530

    Update CORS for production

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,22 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://atsfreak.vercel.app" 
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
     credentials: true
-}))
+}));
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
