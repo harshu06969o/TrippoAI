b1a7fcef4d6df8bdbf377b4a00249e85ef0a65c8
Harshu Chaturvedi <hchaube7@gmail.com>
2026-06-27 19:58:53 +0530

    reverted back to previous code due to error

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,9 +6,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3000', 
         changeOrigin: true,
-      }
-    }
-  }
+        secure: false,
+      },
+    },
+  },
 })
